e906c1f4a03c0f527583513d9a49229499f69b95
fabricio jacob <fabrluc@outlook.com>
2024-05-12 19:53:11 -0300

    add

--- src/main/java/com/github/fabriciolfj/ia/ChatService.java
+++ src/main/java/com/github/fabriciolfj/ia/ChatService.java
@@ -6,7 +6,7 @@ import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Objects;
+
 
 @Service
 @RequiredArgsConstructor
